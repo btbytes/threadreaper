f392e9d39c0471cd01f4f340770add61d0b7273d
Pradeep Gowda <pgowda@proofpoint.com>
2020-05-22 01:26:05 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Thread Reaper
+
+A simple utitlity to convert [Thread Reader](https://threadreaderapp.com/) pages into simpler HTML pages.
+
+Usage:
+	./threadreaper URL ["TITLE"] > local_page.html
+	./threadreaper https://threadreaderapp.com/thread/1202293011150852096.html "On Binbary Numbers" > foone_binary.html
+
+This program also fixes an issue with embedded images that you would have if you were to simply download the Thread Reader HTML to disk.
+
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ThreadReaper(url string, pageTitle string) {
+	// Request the HTML page.
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf(`<!doctype html><html>
+ 
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <style type="text/css">
+  .row {
+    margin-top: 1.5em;
+  }
+  </style>
+  </head>
+  <body>
+  <div class="container">
+  <div class="page-header">
+  `)
+	t := time.Now()
+	fmt.Printf("<h1>%s</h1>", pageTitle)
+	fmt.Printf(`<p><a href="%v">Original Thread</a></p>`, url)
+	fmt.Printf("<time>%s</time>", t.Format("2006-01-02T15:04:05-07:00"))
+	fmt.Println(`</div><!-- page-header -->`)
+
+	// Find the review items
+	doc.Find(".content-tweet").Each(func(i int, s *goquery.Selection) {
+		screenName, e1 := s.Attr("data-screenname")
+		tweet, e2 := s.Attr("data-tweet")
+		s.Find("img").Each(func(ii int, ss *goquery.Selection) {
+			imgURL, imgExists := ss.Attr("data-src")
+			if imgExists {
+				ss.SetAttr("src", imgURL)
+			}
+		})
+		s.Find(".tw-permalink").Remove()
+		res, err := s.Html()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		if e1 && e2 {
+			fmt.Printf(`<div class="row tweet" data-screenname="%s" data-tweet="%s">%s</div>`, screenName, tweet, res)
+		}
+	})
+
+}
+
+func main() {
+	/* var pageTitle string
+		flag.StringVar(&pageTitle, "title", "", "page title")
+	  flag.Parse()
+	*/
+
+	url := os.Args[1]
+	pageTitle := "---"
+	if len(os.Args) > 2 {
+		pageTitle = os.Args[2]
+	}
+	ThreadReaper(url, pageTitle)
+}
