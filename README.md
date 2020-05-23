35f9d34344b4bea252a460bd7a289e01f78c7a1e
Pradeep Gowda <pgowda@proofpoint.com>
2020-05-23 16:08:21 -0400

    Improvements

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A simple utitlity to convert [Thread Reader](https://threadreaderapp.com/) pages
 
 Usage:
 
-	./threadreaper URL ["TITLE"] > local_page.html
-	./threadreaper https://threadreaderapp.com/thread/1202293011150852096.html "On Binbary Numbers" > foone_binary.html
+	./threadreaper [-title TITLE] [-author AUTHOR] [-css CSSURL] URL  > local_page.html
+	./threadreaper -title "On Binbary Numbers" -author foone https://threadreaderapp.com/thread/1202293011150852096.html  > foone_binary.html
 
 This program also fixes an issue with embedded images that you would have if you were to simply download the Thread Reader HTML to disk.
 
+If not provided,  the output file name will be decided based on the author name and the title.
--- main.go
+++ main.go
@@ -1,36 +1,102 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/gosimple/slug"
+	"github.com/manifoldco/promptui"
 )
 
-func ThreadReaper(url string, pageTitle string) {
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
+func Check(e error) {
+	if e != nil {
+		panic(e)
 	}
+}
+
+func GetTheadId(url string) string {
+
+	parts := strings.Split(url, "/")
+	fname := strings.Split(parts[len(parts)-1], ".")[0]
+	return fname
+}
+
+func main() {
+	var author string
+	var title string
+	var css string
+	var ask bool
+	var outFile string
+	flag.StringVar(&author, "author", "", "Name of the author")
+	flag.StringVar(&title, "title", "", "Page title")
+	flag.StringVar(&css, "css", "https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css", "CSS URL")
+	flag.StringVar(&outFile, "out", "", "Output filename")
+	flag.BoolVar(&ask, "p", true, "prompt me for options")
+	flag.Parse()
+	url := flag.Arg(0)
+	if ask {
+		// Author
+		p1 := promptui.Prompt{
+			Label: "Author",
+		}
+		result, err1 := p1.Run()
+		Check(err1)
+		author = result
+		// Title
+		p2 := promptui.Prompt{
+			Label: "Title",
+		}
+		result, err2 := p2.Run()
+		Check(err2)
+		title = result
+
+		// outFile
+		p3 := promptui.Prompt{
+			Label: "Output File",
+		}
+		result, err3 := p3.Run()
+		Check(err3)
+		outFile = result
+	}
+
+	res, err := http.Get(url)
+	Check(err)
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
+	if outFile == "" {
+		values := []string{}
+		values = append(values, slug.Make(author))
+		values = append(values, slug.Make(title))
+		outFile = strings.Join(values, "-")
+		if author == "" && title == "" {
+			outFile = GetTheadId(url)
+		}
+		sLen := len(outFile)
+		if sLen > 40 {
+			sLen = 40
+		}
+		outFile = outFile[:sLen] + ".html"
 	}
-	fmt.Printf(`<!doctype html><html>
- 
+	f, err := os.Create(outFile)
+	Check(err)
+	defer f.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	Check(err)
+	fmt.Fprintf(f, `<!doctype html>
+  <html>
   <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="%s">
   <style type="text/css">
   .row {
     margin-top: 1.5em;
@@ -40,16 +106,15 @@ func ThreadReaper(url string, pageTitle string) {
   <body>
   <div class="container">
   <div class="page-header">
-  `)
+  `, css)
 	t := time.Now()
-	fmt.Printf("<h1>%s</h1>", pageTitle)
-	fmt.Printf(`<p><a href="%v">Original Thread</a></p>`, url)
-	fmt.Printf("<time>%s</time>", t.Format("2006-01-02T15:04:05-07:00"))
-	fmt.Println(`</div><!-- page-header -->`)
-
-	doc.Find(".content-tweet").Each(func(i int, s *goquery.Selection) {
-		screenName, e1 := s.Attr("data-screenname")
-		tweet, e2 := s.Attr("data-tweet")
+	fmt.Fprintf(f, `<h1 class="title">%s</h1>`, title)
+	fmt.Fprintf(f, `<p>by <span class="author"><a href="https://twitter.com/%s">@%s</a></span></p>`, author, author)
+	fmt.Fprintf(f, `</div><!-- page-header -->`)
+	//  doc.Find(".thread-info .time").Each()
+	doc.Find(".t-main .content-tweet").Each(func(i int, s *goquery.Selection) {
+		screenName, _ := s.Attr("data-screenname")
+		tweet, _ := s.Attr("data-tweet")
 		s.Find("img").Each(func(ii int, ss *goquery.Selection) {
 			imgURL, imgExists := ss.Attr("data-src")
 			if imgExists {
@@ -58,22 +123,10 @@ func ThreadReaper(url string, pageTitle string) {
 		})
 		s.Find(".tw-permalink").Remove()
 		res, err := s.Html()
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-		if e1 && e2 {
-			fmt.Printf(`<div class="row tweet" data-screenname="%s" data-tweet="%s">%s</div>`, screenName, tweet, res)
-		}
+		Check(err)
+		fmt.Fprintf(f, `<div class="row tweet" data-screenname="%s" data-tweet="%s">%s</div>`, screenName, tweet, res)
 	})
+	fmt.Fprintf(f, `<footer>Retrieved on <span class="time"><time>%s</time></span>`, t.Format("2006-01-02T15:04:05-07:00"))
+	fmt.Fprintf(f, `, from <a href="%s">threadreaderapp page</a></footer></body></html>`, url)
 
-}
-
-func main() {
-	url := os.Args[1]
-	pageTitle := ""
-	if len(os.Args) > 2 {
-		pageTitle = os.Args[2]
-	}
-	ThreadReaper(url, pageTitle)
 }
